78055b42187e12f7c60fa724bb8551a6ab22a4ee
Christopher Vouga <crvouga@gmail.com>
2022-01-02 22:19:10 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h2">Home</Typography>
+    </Container>
+  );
+}
+
+export default App;
